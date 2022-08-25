1693e12a3a4e8c6fa9099034b391b2365a84fdcd
Chynmn <chynmn0523@gmail.com>
2022-08-25 16:57:54 +0900

    while문

--- Ch7_controlFlow/MyPlayground.playground/Contents.swift
+++ Ch7_controlFlow/MyPlayground.playground/Contents.swift
@@ -17,3 +17,39 @@ for _ in 1...5 {
     count += 1
 }
 
+/*
+ while 조건문 {
+    // 조건문이 true인 동안 실행될 스위프트 구문
+ }
+ */
+
+var myCount = 0
+
+while myCount < 100 {
+    myCount += 1
+}
+
+/*
+ repeat...while 반복문 <- do..while문 대체
+ repeat {
+    // 적어도 한 번은 실행되어야 하는 스위프트 구문 ex) 배열 안의 항목들 중에 특정 항목을 찾을 떄까지 둘러봐야할 경우
+ } while 조건식
+ */
+
+var i = 10
+repeat {
+    i -= 1
+} while (i > 0)
+
+// 반복문에서 빠져나오기
+var j = 10
+
+for _ in 0 ..< 100 {
+    j += j
+    
+    if j > 100 {
+        break
+    }
+    print("j = \(j)")
+}
+
